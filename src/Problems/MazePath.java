1fbfacc658048cfbc9bbef2a7a8e2fafa63d8886
cybermad7373 <cybermad7373@gmail.com>
2024-06-18 18:21:25 +0530

    new

--- src/Problems/MazePath.java
+++ src/Problems/MazePath.java
@@ -9,4 +9,22 @@ package Problems;
 //  +--------------+         V H H V
 
 public class MazePath {
+
+    public static void Path(int n,int row,int col,String ans) {
+        if(row == n && col == n)
+        {
+            System.out.println(ans);
+            return;
+        }
+        if(row> n|| col >n)
+        {
+            return;
+        }
+        Path(n,row,col+1,ans+"H");
+        Path(n,row+1,col,ans+"V");
+
+    }
+    public static void main(String[] args) {
+        Path(2,0,0,"");
+    }
 }
